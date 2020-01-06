c3d0cdc2b8f051432dc0646853c6d1d05f06e38f
Brian <bmoses00@stuy.edu>
2020-01-06 14:47:35 -0500

    Minor channge to db, does not work

--- db/db_utils.py
+++ db/db_utils.py
@@ -5,8 +5,8 @@ def add_user(username, password):
     db = sqlite3.connect("data/database.db")
     c = db.cursor()
 
-    command = 'INSERT INTO users VALUES ({}, {}, {}, {}, {})'
-    command = command.format(user, passw, ip, lon, lat)
+    command = 'INSERT INTO users VALUES ({}, {}, {}, {})'
+    command = command.format(username, password, full_name)
     c.execute(command)
 
     db.commit()
