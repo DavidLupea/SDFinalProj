59cf8da411d9637a7111a038f8e11f144f008643
Mabedin00 <mabedin00@stuy.edu>
2020-01-08 09:07:41 -0500

    database creation

--- db/db_builder.py
+++ db/db_builder.py
@@ -9,4 +9,29 @@ def create_tables():
     full_name TEXT,
     password TEXT
     );""")
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS user_profile(
+    project TEXT PRIMARY KEY,
+    reddit_posts INT,
+    address TEXT
+    );""")
+    c.close()
+
+def create_projects(owner, projectname):
+    db = sqlite3.connect("database.db")
+    c = db.cursor()
+    projectname = owner + "_" + projectname
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS {}(
+    member TEXT PRIMARY KEY,
+    task_name TEXT,
+    task_desc TEXT,
+    task_assigned TEXT,
+    task_status TEXT,
+    task_due_date TEXT,
+    meeting_desc TEXT,
+    meeting_location TEXT,
+    meeting_date TEXT
+    );""")
     c.close()
